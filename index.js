9addcdf5f8e82230b53b2e09e67292c8d22f845a
yl1462 <yliang1462@hotmail.com>
2020-08-28 16:38:40 -0500

    finally works

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 $("#js-shopping-list-form").on("submit", function(event){
   event.preventDefault();
   const text = $("#shopping-list-entry").val();
-  $(".shopping-list").append($("<li>").append(text));
+
   $("#shopping-list-entry").val("");
+
+  $(".shopping-list").append(
+    `<li>
+      <span class="shopping-item">${text}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`);
+  
+});
+
+$('.shopping-list').on('click','.shopping-item-toggle', function(event) {
+  $(this).closest(`li`).children(".shopping-item").toggleClass("shopping-item__checked");
 })
 
-$('.container').on('click','.shopping-item-delete', function() {
-  $(this).remove(event.currectTarget);
+$('.shopping-list').on('click','.shopping-item-delete', function(event) {
+  $(this).closest(`li`).remove();
 })
+
